03617be47f49cbf6a1c34cbe39cebfb3b4753e2c
PHPBOSS1 <kkonstantinov926@gmail,com>
2020-01-20 15:00:47 +0200

    логин

--- src/login.php
+++ src/login.php
@@ -4,8 +4,8 @@ namespace MyProject;
 session_start();
 ?>
 
-<?php require_once("src/includes/connection.php"); ?>
-<?php include("src/includes/header.php"); ?>
+<?php require_once("includes/connection.php"); ?>
+<?php include("includes/header.php"); ?>
 <?php
 
 if(isset($_SESSION["session_username"])){
@@ -43,3 +43,17 @@ if(isset($_POST["login"])){
     }
 }
 ?>
+<div class="container mlogin">
+    <div id="login">
+        <h1>Вход</h1>
+        <form action="" id="loginform" method="post"name="loginform">
+            <p><label for="user_login">Имя пользователя<br>
+                    <input class="input" id="username" name="username"size="20"
+                           type="text" value=""></label></p>
+            <p><label for="user_pass">Пароль<br>
+                    <input class="input" id="password" name="password"size="20"
+                           type="password" value=""></label></p>
+            <p class="submit"><input class="button" name="login"type= "submit" value="Log In"></p>
+            <p class="regtext">Еще не зарегистрированы?<a href= "register.php">Регистрация</a>!</p>
+        </form>
+    </div>
